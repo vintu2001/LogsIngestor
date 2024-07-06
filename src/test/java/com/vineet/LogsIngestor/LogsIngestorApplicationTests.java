9beca69823fb117cdbcaf6aad17dc9b0fce063df
Vineet Malewar <malewar.vineet2001@gmail.com>
2024-07-06 17:00:15 +0530

    Initial commit

---
+++ src/test/java/com/vineet/LogsIngestor/LogsIngestorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vineet.LogsIngestor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LogsIngestorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
